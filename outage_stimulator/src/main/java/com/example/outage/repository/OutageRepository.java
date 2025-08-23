6c8b115736b7bf95070361fa4cd2ab682e03711c
Sai-Akshaya-L <144267027+Sai-Akshaya-L@users.noreply.github.com>
2025-08-23 18:28:13 +0530

    Delete outage_stimulator directory

--- outage_stimulator/src/main/java/com/example/outage/repository/OutageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.outage.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.outage.model.OutageModel;
-
-public interface OutageRepository extends JpaRepository<OutageModel,String>{
-
-}
